21b0ffdf92e5d1d8661e2711d928ae4c6256f6d3
Mia <miacrandell@gmail.com>
2018-03-21 16:31:17 -0600

    minor css and html changes

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,8 +1,8 @@
 $.getJSON("/headlines", function(data) {
     for (var i = 0; i < data.length; i++) {
         $(".headlineList").append(
-            "<div class='headline'><h3 class='headlineTitle uk-align-center'><a href='" + data[i].link + " 'target='_blank'>" + data[i].headline + "</a><button class='uk-button uk-button uk-float-right uk-box-shadow-large saveButton'><span class='saveButtonText'>Save Article</span></button></h3></div><div class='description'><p class='descriptionParagraph uk-align-center'>" + data[i].teaser + "</p></div>");
-            console.log(data[i].link);
+            "<div class='headline'><h4 class='headlineTitle uk-align-center'><a href=" + data[i].link + " target='_blank' class='linkText'>" + data[i].headline + "</a><button class='uk-button uk-button uk-align-right saveButton'><span class='saveButtonText'>Save Article</span></button></h4></div><div class='description'><p class='descriptionParagraph uk-align-center'>" + data[i].teaser + "</p></div>");
+            console.log("The link is: " + data[i].link);
     }
 });
 
